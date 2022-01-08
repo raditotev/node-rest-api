09e59d789482709efbc1a0ef828f780883742426
Radi Totev <raditotev@gmail.com>
2022-01-08 11:49:23 +0200

    Add logging

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./routes/products');
 const orderRoutes = require('./routes/orders');
+
+app.use(morgan('dev'));
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
