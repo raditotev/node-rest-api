15f5e402d551b561ff53ce78cfb6d6f6b803c1b2
Radi Totev <raditotev@gmail.com>
2022-01-08 12:03:13 +0200

    Add error handling

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const createError = require('http-errors');
 
 const productRoutes = require('./routes/products');
 const orderRoutes = require('./routes/orders');
@@ -9,5 +10,16 @@ app.use(morgan('dev'));
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new createError.NotFound();
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res
+    .status(error.status || 500)
+    .json({ message: error.message || 'Server error' });
+});
 
 app.listen(3000);
