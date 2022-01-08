d63b5adb1b1d0bd1ac1a1c177f39a51f48e52cf0
Radi Totev <raditotev@gmail.com>
2022-01-08 13:02:28 +0200

    Add create an order endpoint

--- routes/orders.js
+++ routes/orders.js
@@ -5,6 +5,12 @@ router.get('/', (req, res, next) => {
   res.status(200).json({ message: 'GET all orders' });
 });
 
+router.post('/', (req, res, next) => {
+  const { pid, quantity } = req.body;
+  const order = { pid, quantity };
+  res.status(201).json({ message: 'Created new order', order });
+});
+
 router.get('/:orderId', (req, res, next) => {
   const { orderId } = req.params;
   res.status(200).json({ message: `GET order with id ${orderId}` });
