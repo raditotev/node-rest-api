a1930c0ecd028f7e57fcb59eaefd7cfd2f03d0aa
Radi Totev <raditotev@gmail.com>
2022-01-09 12:55:45 +0200

    Connect rest of product endpoints with DB

--- controllers/products-controller.js
+++ controllers/products-controller.js
@@ -28,19 +28,72 @@ const createProduct = async (req, res, next) => {
   }
 };
 
-const getProduct = (req, res, next) => {
+const getProduct = async (req, res, next) => {
   const id = req.params.productId;
-  res.status(200).json({ message: `Get product with an ID ${id}` });
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      return next(createError(422, 'This product ID does not exists'));
+    }
+    res.status(200).json({ message: `Get product with an ID ${id}`, product });
+  } catch (error) {
+    console.log(error);
+    next(createError(502, error.message));
+  }
 };
 
-const updateProduct = (req, res, next) => {
+const updateProduct = async (req, res, next) => {
   const id = req.params.productId;
-  res.status(200).json({ message: `Update product with an ID of ${id}` });
+  const { name, price } = req.body;
+
+  let product;
+  try {
+    product = await Product.findById(id);
+  } catch (error) {
+    console.log(error);
+    return next(createError(502, error.message));
+  }
+
+  if (!product) {
+    return next(createError(422, 'This product ID does not exists'));
+  }
+
+  product.name = name ? name : product.name;
+  product.price = price ? price : product.price;
+
+  try {
+    await product.save();
+    res
+      .status(200)
+      .json({ message: `Update product with an ID of ${id}`, product });
+  } catch (error) {
+    console.log(error);
+    next(createError(502, error.message));
+  }
 };
 
-const deleteProduct = (req, res, next) => {
+const deleteProduct = async (req, res, next) => {
   const id = req.params.productId;
-  res.status(200).json({ message: `Delete product with an ID of ${id}` });
+
+  let product;
+  try {
+    product = await Product.findById(id);
+  } catch (error) {
+    console.log(error);
+    return next(createError(502, error.message));
+  }
+
+  if (!product) {
+    return next(createError(422, 'This product ID does not exists'));
+  }
+
+  try {
+    await product.remove();
+    res.status(200).json({ message: `Delete product with an ID of ${id}` });
+  } catch (error) {
+    console.log(error);
+    return next(createError(502, error.message));
+  }
 };
 
 module.exports = {
