2ae309606ccb2f1291e5119be4fba3a64c39cbff
Radi Totev <raditotev@gmail.com>
2022-01-09 13:06:49 +0200

    Connect rest of order endpoints with db

--- controllers/orders-controller.js
+++ controllers/orders-controller.js
@@ -25,14 +25,36 @@ const createOrder = async (req, res, next) => {
   }
 };
 
-const getOrder = (req, res, next) => {
+const getOrder = async (req, res, next) => {
   const { orderId } = req.params;
-  res.status(200).json({ message: `GET order with id ${orderId}` });
+
+  try {
+    const order = await Order.findById(orderId);
+    if (!order) {
+      return next(createError(422, 'No order with this ID'));
+    }
+    res.status(200).json({ message: `GET order with id ${orderId}`, order });
+  } catch (error) {
+    console.log(error);
+    next(createError(502, error.message));
+  }
 };
 
-const deleteOrder = (req, res, next) => {
+const deleteOrder = async (req, res, next) => {
   const { orderId } = req.params;
-  res.status(200).json({ message: `DELETE order with id ${orderId}` });
+
+  try {
+    const order = await Order.findById(orderId);
+    if (!order) {
+      return next(createError(422, 'No order with this ID'));
+    }
+
+    await order.remove();
+    res.status(200).json({ message: `DELETE order with id ${orderId}` });
+  } catch (error) {
+    console.log(error);
+    next(createError(502, error.message));
+  }
 };
 
 module.exports = {
