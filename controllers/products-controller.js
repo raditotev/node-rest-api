771f1653ec6e637e63f2698804f837e26b72b6ca
Radi Totev <raditotev@gmail.com>
2022-01-09 13:13:02 +0200

    Refactor/improve update and delete product endpoints

--- controllers/products-controller.js
+++ controllers/products-controller.js
@@ -46,48 +46,35 @@ const updateProduct = async (req, res, next) => {
   const id = req.params.productId;
   const { name, price } = req.body;
 
-  let product;
   try {
-    product = await Product.findById(id);
-  } catch (error) {
-    console.log(error);
-    return next(createError(502, error.message));
-  }
+    const product = await Product.findById(id);
 
-  if (!product) {
-    return next(createError(422, 'This product ID does not exists'));
-  }
+    if (!product) {
+      return next(createError(422, 'This product ID does not exists'));
+    }
 
-  product.name = name ? name : product.name;
-  product.price = price ? price : product.price;
+    product.name = name ? name : product.name;
+    product.price = price ? price : product.price;
 
-  try {
     await product.save();
     res
       .status(200)
       .json({ message: `Update product with an ID of ${id}`, product });
   } catch (error) {
     console.log(error);
-    next(createError(502, error.message));
+    return next(createError(502, error.message));
   }
 };
 
 const deleteProduct = async (req, res, next) => {
   const id = req.params.productId;
 
-  let product;
   try {
-    product = await Product.findById(id);
-  } catch (error) {
-    console.log(error);
-    return next(createError(502, error.message));
-  }
+    const product = await Product.findById(id);
+    if (!product) {
+      return next(createError(422, 'This product ID does not exists'));
+    }
 
-  if (!product) {
-    return next(createError(422, 'This product ID does not exists'));
-  }
-
-  try {
     await product.remove();
     res.status(200).json({ message: `Delete product with an ID of ${id}` });
   } catch (error) {
