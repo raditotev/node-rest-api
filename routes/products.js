4e1583de6fe927ce363190178d5412351f97578a
Radi Totev <raditotev@gmail.com>
2022-01-13 18:55:25 +0200

    Set multer as a middleware to handle file uploads

--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,43 @@
 const express = require('express');
-
-const productsController = require('../controllers/products-controller');
+const multer = require('multer');
 
 const router = express.Router();
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'tmp/');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().getTime() + file.originalname);
+  },
+});
+const fileFilter = (req, file, cb) => {
+  const mimetype = file.mimetype;
+  if (
+    mimetype === 'image/png' ||
+    mimetype === 'image/jpg' ||
+    mimetype === 'image/jpeg' ||
+    mimetype === 'image/gif'
+  ) {
+    cb(null, true);
+  } else {
+    cb(
+      new Error(
+        'Unsupported file type. Supported types: jpeg, jpg, gif and png'
+      )
+    );
+  }
+};
+const limits = {
+  fileSize: 1024 * 1024 * 3,
+};
+const upload = multer({ storage, fileFilter, limits });
+
+const productsController = require('../controllers/products-controller');
+
 router.get('/', productsController.getProducts);
 
-router.post('/', productsController.createProduct);
+router.post('/', upload.single('image'), productsController.createProduct);
 
 router.get('/:productId', productsController.getProduct);
 
