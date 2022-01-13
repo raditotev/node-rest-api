d27a6acdd75ccee3f126e93a4543b9c76d5c8a39
Radi Totev <raditotev@gmail.com>
2022-01-13 19:01:45 +0200

    Update Product model with image property

--- models/product.js
+++ models/product.js
@@ -3,6 +3,7 @@ const { Schema, model } = require('mongoose');
 const productSchema = new Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
+  image: { type: String },
 });
 
 module.exports = model('Product', productSchema);
