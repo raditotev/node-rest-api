b83a2c4e6b2bc4abfae47177b8a405425610c29e
Radi Totev <raditotev@gmail.com>
2022-01-16 20:38:50 +0200

    Helper generating valid jwt token

---
+++ __tests__/helpers/jwt-token.js
@@ -0,0 +1,11 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = () => {
+  const payload = {
+    userId: '61e05744a2f380b559cf40a7',
+    email: 'test@example.com',
+  };
+
+  const secret = process.env.JWT_SECRET;
+  return jwt.sign(payload, secret, { expiresIn: '1h' });
+};
