793e40c151be88b76cebe4c1c476fb1b47a36d5e
Radi Totev <raditotev@gmail.com>
2022-01-16 20:40:05 +0200

    Test GET /products/:id endpoint

---
+++ __tests__/products/get.product.test.js
@@ -0,0 +1,65 @@
+const request = require('supertest');
+const app = require('../../app');
+const Product = require('../../models/product');
+
+Product.findById = jest.fn();
+
+const mockProduct = {
+  _id: '61e05744a2f380b559cf40a7',
+  name: 'apples',
+  price: 1.29,
+};
+
+describe('GET /product/:id', () => {
+  test('fetch product successfully', async () => {
+    expect.assertions(4);
+
+    Product.findById.mockReturnValueOnce({
+      select: async () => Promise.resolve(mockProduct),
+    });
+
+    const response = await request(app).get(`/products/${mockProduct._id}`);
+
+    expect(Product.findById).toHaveBeenCalledTimes(1);
+    expect(response.statusCode).toBe(200);
+    expect(response.body.message).toBe(
+      `Get product with an ID ${mockProduct._id}`
+    );
+    expect(response.body.product).toEqual(mockProduct);
+  });
+
+  test('invalid params', async () => {
+    expect.assertions(3);
+
+    const response = await request(app).get('/products/invalid-param');
+
+    expect(Product.findById).toHaveBeenCalledTimes(0);
+    expect(response.statusCode).toBe(400);
+    expect(response.body.message).toMatchInlineSnapshot(`"Invalid product ID"`);
+  });
+  test('missing product', async () => {
+    expect.assertions(2);
+
+    Product.findById.mockReturnValueOnce({
+      select: async () => Promise.resolve(null),
+    });
+
+    const response = await request(app).get(`/products/${mockProduct._id}`);
+
+    expect(response.statusCode).toBe(422);
+    expect(response.body.message).toBe('This product ID does not exists');
+  });
+  test('server error', async () => {
+    expect.assertions(2);
+
+    const mockError = new Error('Failed');
+    Product.findById.mockReturnValueOnce({
+      select: async () => Promise.reject(mockError),
+    });
+
+    const response = await request(app).get(`/products/${mockProduct._id}`);
+
+    expect(response.statusCode).toBe(502);
+    expect(response.body.message).toBe(mockError.message);
+  });
+});
