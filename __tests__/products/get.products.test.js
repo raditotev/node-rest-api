4978126636a9e05f78f486276db8df4a1e349e9e
Radi Totev <raditotev@gmail.com>
2022-01-17 12:50:00 +0200

    Update/refactor get products tests

--- __tests__/products/get.products.test.js
+++ __tests__/products/get.products.test.js
@@ -7,11 +7,22 @@ Product.find = jest.fn();
 describe.only('GET /products', () => {
   test('get products', async () => {
     expect.assertions(3);
+
     const mockProducts = [
       {
-        _id: 'abc',
-        name: 'test',
+        _id: '123',
+        name: 'Apple',
         price: 1.29,
+      },
+      {
+        _id: '234',
+        name: 'Pear',
+        price: 1.69,
+      },
+      {
+        _id: '345',
+        name: 'Orange',
+        price: 0.99,
       },
     ];
     Product.find.mockReturnValueOnce({
@@ -19,6 +30,7 @@ describe.only('GET /products', () => {
     });
 
     const response = await request(app).get('/products');
+
     expect(Product.find).toHaveBeenCalledTimes(1);
     expect(response.statusCode).toBe(200);
     expect(response.body.products).toEqual(mockProducts);
