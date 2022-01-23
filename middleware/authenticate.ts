7b79c48d763943c8985f6c5b0f55563474aebcc3
Radi Totev <raditotev@gmail.com>
2022-01-23 14:38:04 +0200

    Resolve problems in authenticate middleware
    * Extend expess Request interface with a payload property, so that a decoded token payload can be attached to request object
    * Define UserData type
    * if there is no autorization in the headers or there is no token move to the next middleware

--- middleware/authenticate.ts
+++ middleware/authenticate.ts
@@ -1,15 +1,37 @@
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import createError from 'http-errors';
 import express from 'express';
+
+type UserData = {
+  userId: string;
+  email: string;
+};
+
+declare global {
+  namespace Express {
+    interface Request {
+      payload: UserData;
+    }
+  }
+}
 
 export default (
   req: express.Request,
   res: express.Response,
   next: express.NextFunction
 ) => {
+  if (!req.headers.authorization) {
+    return next();
+  }
+
   const token = req.headers.authorization.split(' ')[1];
+
+  if (!token) {
+    return next();
+  }
+
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!) as UserData;
     req.payload = decoded;
   } catch (error) {
     console.log(error);
